6fc8361156bfd2fde1964e6982cf7d9cb642d0f4
Sh1nroq <dorovskih2005@gmail.com>
2025-10-12 22:35:40 +0300

    -added bd

--- parser/json_parser.py
+++ parser/json_parser.py
@@ -1,14 +1,16 @@
 import orjson
 
-with open("../data/anime.json", "r", encoding="utf8") as f:
-    data = orjson.loads(f.read())
+def json_parser(filepath: str):
+    with open(filepath, "r", encoding="utf8") as f:
+        data = orjson.loads(f.read())
 
-# print("mal_id:", data[0]["mal_id"])
-parsed_info = {}
+    # print("mal_id:", data[0]["mal_id"])
+    parsed_info = {}
 
-for record in data:
-    genres = record.get("genres", [])
-    mal_id = record.get("mal_id")
-    synopsis = record.get("synopsis", "")
-    parsed_info[mal_id] = {"genres": [g["name"] for g in genres], "synopsis": synopsis}
-print(parsed_info, "record")
+    for record in data:
+        genres = record.get("genres", [])
+        mal_id = record.get("mal_id")
+        synopsis = record.get("synopsis", "")
+        parsed_info[mal_id] = {"genres": [g["name"] for g in genres], "synopsis": synopsis}
+    #print(parsed_info, "record")
+    return parsed_info
