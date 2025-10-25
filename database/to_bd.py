0b2821180dc793631c397c0d83068354d8eb4af6
Sh1nroq <dorovskih2005@gmail.com>
2025-10-25 22:00:29 +0300

    Get working model

--- database/to_bd.py
+++ database/to_bd.py
@@ -8,34 +8,35 @@ from urllib.parse import quote_plus
 from sqlalchemy import Text
 from sqlalchemy.dialects.postgresql import JSONB
 
-user = quote_plus('postgres')
-password = quote_plus('123321')  # если есть спецсимволы, они будут закодированы
-host = '127.0.0.1'
-port = 5432
-database = 'anime_information'
+def info_to_postgres():
+    user = quote_plus('postgres')
+    password = quote_plus('123321')  # если есть спецсимволы, они будут закодированы
+    host = '127.0.0.1'
+    port = 5432
+    database = 'anime_information'
 
-engine = create_engine(
-    f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}",
-    echo=True
-)
+    engine = create_engine(
+        f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}",
+        echo=True
+    )
 
-metadata = MetaData()
+    metadata = MetaData()
 
-anime_table = Table(
-    "anime",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("data", JSONB),
-    Column("last_updated", TIMESTAMP, server_default=text("CURRENT_TIMESTAMP")),
-)
+    anime_table = Table(
+        "anime",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("data", JSONB),
+        Column("last_updated", TIMESTAMP, server_default=text("CURRENT_TIMESTAMP")),
+    )
 
-metadata.create_all(engine)
+    metadata.create_all(engine)
 
-anime_info = json_parser("../data/anime.json")
+    anime_info = json_parser("../data/anime.json")
 
-with Session(engine) as session:
-    for mal_id, info in anime_info.items():
-        insert_anime_to_bd = insert(anime_table).values(
-            id=mal_id, data=info).on_conflict_do_nothing()
-        session.execute(insert_anime_to_bd)
-    session.commit()
+    with Session(engine) as session:
+        for mal_id, info in anime_info.items():
+            insert_anime_to_bd = insert(anime_table).values(
+                id=mal_id, data=info).on_conflict_do_nothing()
+            session.execute(insert_anime_to_bd)
+        session.commit()
--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-
 from database.from_bd import get_info_from_bd
 from model.utils import preprocessing_data
 
-def main():
+titles, genres, synopsis = get_info_from_bd()
+preprocessing_data(titles, genres, synopsis)
 
-    titles, genres, synopsis = get_info_from_bd()
-    preprocessing_data(titles, genres, synopsis)
-
-
-
-if __name__ == "__main__":
-    main()
 
